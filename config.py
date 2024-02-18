cc6b6ea7bc9df2e282f6f73ac54c1a72bb6599c4
Firos sha muhammad <96859506+firossha321@users.noreply.github.com>
2024-02-18 19:44:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-
-BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+API_ID= 17432758
+API_HASH= c9e31fda0ce722e3f3033a9d4f140783
+BOT_TOKEN=6912468326:AAHIPW94tdYfybtiqOftDFU5aiWjw9jFFNY
+OWNER_ID=6666362904
+MONGO_DB_URI=mongodb+srv://firossha321:firossha321@cluster0.xdtpx.mongodb.net/?retryWrites=true&w=majority
+SUPPORT_CHAT ="https://t.me/firossha"
