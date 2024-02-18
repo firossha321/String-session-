9d1d1e598bfa43e1340b4f6e524ad9a843351d12
Firos sha muhammad <96859506+firossha321@users.noreply.github.com>
2024-02-18 19:49:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 
 API_ID= 17432758
-API_HASH= c9e31fda0ce722e3f3033a9d4f140783
-BOT_TOKEN=6912468326:AAHIPW94tdYfybtiqOftDFU5aiWjw9jFFNY
+API_HASH= 'c9e31fda0ce722e3f3033a9d4f140783'
+BOT_TOKEN="6912468326:AAHIPW94tdYfybtiqOftDFU5aiWjw9jFFNY"
 OWNER_ID=6666362904
-MONGO_DB_URI=mongodb+srv://firossha321:firossha321@cluster0.xdtpx.mongodb.net/?retryWrites=true&w=majority
+MONGO_DB_URI="mongodb+srv://firossha321:firossha321@cluster0.xdtpx.mongodb.net/?retryWrites=true&w=majority"
 SUPPORT_CHAT ="https://t.me/firossha"
